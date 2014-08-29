3c21830762b77b50d2491911feff4aa828f5204e
ashokgujju <ashokg.geek@gmail.com>
2014-08-29 18:13:28 +0530

    scan devices

--- src/com/as/mybluetooth/MainActivity.java
+++ src/com/as/mybluetooth/MainActivity.java
@@ -1,11 +1,13 @@
 package com.as.mybluetooth;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -22,20 +24,23 @@ public class MainActivity extends ActionBarActivity {
 	private ListView lv;
 	private BluetoothAdapter BA;
 	private Set<BluetoothDevice> pairedDevices;
-	
+	ArrayAdapter adapter;	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 		On = (Button) findViewById(R.id.button1);
 		Visible = (Button) findViewById(R.id.button2);
 		list = (Button) findViewById(R.id.button3);
 		Off = (Button) findViewById(R.id.button4);
-		
 		lv = (ListView) findViewById(R.id.listView1);
 		
 		BA = BluetoothAdapter.getDefaultAdapter();
 		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		lv.setAdapter(adapter);
 	}
 
 	public void on(View view) {
@@ -45,8 +50,40 @@ public class MainActivity extends ActionBarActivity {
 			Toast.makeText(this, "Turned On", Toast.LENGTH_LONG).show();
 		}
 		else {
-			;
+			Toast.makeText(this, "Already Turned On", Toast.LENGTH_LONG).show();
 		}
+	}	
+	
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        // When discovery finds a device
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            // Get the BluetoothDevice object from the Intent
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            // Add the name and address to an array adapter to show in a ListView
+	            adapter.add(device.getName() + "\n" + device.getAddress());
+	            adapter.notifyDataSetChanged();
+	        }
+	    }
+	};
+	
+	public void findDevices(View view) {
+		if (BA.isDiscovering()) {
+			BA.cancelDiscovery();
+		}
+		else {
+			adapter.clear();
+			BA.startDiscovery();
+			
+			registerReceiver(mReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(mReceiver);
 	}
 	
 	public void visible(View view){
@@ -56,13 +93,9 @@ public class MainActivity extends ActionBarActivity {
 	
 	public void list(View view) {
 		pairedDevices = BA.getBondedDevices();
-		
-		ArrayList list = new ArrayList();
+
 		for(BluetoothDevice bt: pairedDevices)
-			list.add(bt.getName());
-		
-		final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,list);
-		lv.setAdapter(adapter);
+			adapter.add(bt.getName()+"\n"+bt.getAddress());
 	}
 	
 	public void off(View view) {
